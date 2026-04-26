369d2d39ba894b9f00c5f34145408e5752c5cfa9
Zahary Shinikchiev <shinikchiev@yahoo.com>
2026-04-26 09:56:04 +0300

    Delete src/index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-export { DeviceTier, detectDeviceTier } from './deviceTier.js';
-export { createSpriteCacheForTier, getTierDefaults, createCacheAuto } from './presets.js';
-export { AssetCategory, EVICTION_ORDER, CategoryRegistry } from './categoryRegistry.js';
-export { VramManager } from './vramManager.js';
-export { loadScene, unloadScene, transitionScenes, preloadScene, cleanupPartialLoad } from './sceneStreaming.js';
-export { AssetResolver, resolveTextureUrl } from './assetResolver.js';
-export { VramHUD } from './vramHud.js';
-export { runSafariStressTest, runResolutionSweep } from './safariStressTest.js';
